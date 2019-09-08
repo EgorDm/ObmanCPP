a18ccefead8dcf76a4738f456ecf5fe9857c9ad5
Egor Dmitriev <egordmitriev2@gmail.com>
2019-09-08 23:04:07 +0200

    Implemented object manager

--- object_manager/implementation/object_manager.cpp
+++ object_manager/implementation/object_manager.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "object_manager.h"
+#include "../utils.h"
+
+using namespace object_manager;
+
+Instance ObjectManager::create(const KindId &id, std::unordered_map<std::string, Argument> &arguments) {
+    auto pref_id = config->get_preference(id);
+    return factory->create(pref_id, arguments);
+}
+
+Instance ObjectManager::create(KindAccessor type, std::unordered_map<std::string, Argument> &arguments) {
+    auto id = config->get_kind_table().get_id_or_insert(utils::trim_slash(type));
+    return create(id, arguments);
+}
+
+Instance &ObjectManager::get(const KindId &id) {
+    if(shared_instances.find(id) == shared_instances.end()) {
+        std::map<std::string, Php::Value> args;
+        shared_instances[id] = create(id, args);
+    }
+
+    return shared_instances[id];
+}
+
+Instance &ObjectManager::get(KindAccessor type) {
+    auto id = config->get_kind_table().get_id_or_insert(utils::trim_slash(type));
+    return get(id);
+}
+
+void ObjectManager::configure(std::map<std::string, Php::Value> &configuration) {
+    config->extend(configuration);
+}
--- object_manager/implementation/object_manager.h
+++ object_manager/implementation/object_manager.h
@@ -4,9 +4,34 @@
 
 #pragma once
 
+#include "../interfaces/object_manager_interface.h"
+#include "config.h"
+#include "factory.h"
 
-class object_manager {
+using namespace object_manager::interface;
+using namespace object_manager::structures;
 
+namespace object_manager {
+    class ObjectManager : public ObjectManagerInterface {
+    protected:
+        Config* config; // TODO: managed by php!
+        Factory* factory;
+
+        std::unordered_map<KindId, Instance> shared_instances;
+    public:
+        ObjectManager(Config *config, Factory *factory, const std::unordered_map<KindId, Instance> &shared_instances)
+                : config(config), factory(factory), shared_instances(shared_instances) {}
+
+        Instance create(const KindId &id, std::unordered_map<std::string, Argument> &arguments) override;
+
+        Instance create(KindAccessor type, std::unordered_map<std::string, Argument> &arguments) override;
+
+        Instance &get(const KindId &id) override;
+
+        Instance &get(KindAccessor type) override;
+
+        void configure(std::map<std::string, Php::Value> &configuration) override;
+    };
 };
 
 
